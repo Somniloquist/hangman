903b177fb55ceb83b4029d12201b06a7f39bf7e3
Robin Unger <robin.e.unger@gmail.com>
2019-03-28 15:36:29 -0500

    Add functions to get guesses from player

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,7 @@
 
 class Game
   attr_reader :dictionary, :min_length, :max_length, :max_guesses, :secret_word
-  attr_accessor :guesses
+  attr_accessor :guesses, :hidden_word, :used_letters
   def initialize(dictionary, input = {})
     @dictionary = dictionary
     @min_length = input.fetch(:min_length, 1)
@@ -10,14 +10,47 @@ class Game
     @max_guesses = input.fetch(:max_guesses, 6)
     @guesses = 0
     @secret_word = input.fetch(:secret_word, set_secret_word)
+    @hidden_word = "_" * secret_word.length
+    @used_letters = []
+  end
+
+  public
+  def play
+    max_guesses.times do 
+      puts("Word:\t#{@hidden_word}")
+      puts("Miss:\t#{@used_letters.join}")
+      guess = solicit_guess
+    end
+    puts("GAME OVER")
   end
 
   private
-  def set_secret_word
-    @dictionary.select { |word| word.length >= @min_length && word.length <= @max_length}.sample
+  def solicit_guess
+    loop do
+      print("Guess a letter: ")
+      letter = gets.chomp
+      return letter if valid_guess?(letter)
+    end
   end
+
+  def valid_guess?(string)
+    string.length == 1 && string.match(/[a-z]/) ? true : false
+  end
+
+  def set_secret_word
+    @dictionary.select { |word| word.length >= @min_length && word.length <= @max_length}.sample.downcase
+  end
+
+  def game_won?
+    @secret_word == @guess
+  end
+
 end
 
 dictionary = File.read("dictionary.txt").split
 game = Game.new(dictionary)
-puts("Word: #{game.secret_word}")
+puts("Welcome to hangman!")
+game.play
+
+# puts("DEBUG - Word:\t#{game.secret_word}")
+# puts("DEBUG - Guess:\t#{game.guess}")
