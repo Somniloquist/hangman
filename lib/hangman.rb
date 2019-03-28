bd50952f269d8d42268b0c1756ffc130799af016
Robin Unger <robin.e.unger@gmail.com>
2019-03-28 14:47:58 -0500

    Refactor functions into game class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/env ruby
-puts "Welcome to Hangman!"
 
-def get_random_word(word_array, min_length = 1, max_length = 12)
-  word_array.select { |word| word.length >= min_length && word.length <= max_length}.sample
+class Game
+  attr_reader :dictionary, :min_length, :max_length, :max_guesses, :secret_word
+  attr_accessor :guesses
+  def initialize(dictionary, input = {})
+    @dictionary = dictionary
+    @min_length = input.fetch(:min_length, 1)
+    @max_length = input.fetch(:max_length, 12)
+    @max_guesses = input.fetch(:max_guesses, 6)
+    @guesses = 0
+    @secret_word = input.fetch(:secret_word, set_secret_word)
+  end
+
+  private
+  def set_secret_word
+    @dictionary.select { |word| word.length >= @min_length && word.length <= @max_length}.sample
+  end
 end
 
 dictionary = File.read("dictionary.txt").split
-secret_word = get_random_word(dictionary)
-
-puts("Word: #{secret_word}")
+game = Game.new(dictionary)
+puts("Word: #{game.secret_word}")
