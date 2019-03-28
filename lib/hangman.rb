578e1d0e29e360f6f557623e9fc34780cd8ff3c3
Robin Unger <robin.e.unger@gmail.com>
2019-03-28 14:03:57 -0500

    Open word list and select a random word

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 puts "Welcome to Hangman!"
+
+def get_random_word(word_array, min_length = 1, max_length = 12)
+  word_array.select { |word| word.length >= min_length && word.length <= max_length}.sample
+end
+
+dictionary = File.read("dictionary.txt").split
+secret_word = get_random_word(dictionary)
+
+puts("Word: #{secret_word}")
