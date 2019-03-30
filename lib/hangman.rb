3966ac0b2e68a92054102d51ca1cf52bd72bfe4f
Robin Unger <robin.e.unger@gmail.com>
2019-03-30 13:41:31 -0500

    Allow multiple save files

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 require "yaml"
+require "fileutils"
 
 class Game
+  SAVE_FILE_DIR = "./save/"
   attr_reader :dictionary, :secret_word
   attr_accessor :guesses, :hidden_word, :used_letters
   def initialize(input = {})
@@ -44,10 +46,13 @@ class Game
   private
   def save_game
     yaml = YAML::dump(self)
-    save_file = File.open("save.yaml", "w")
-    save_file.puts(yaml)
+    FileUtils.mkdir(SAVE_FILE_DIR) unless File.directory?(SAVE_FILE_DIR)
+    File.open("#{SAVE_FILE_DIR}/save#{save_file_count}.yaml", "w") {|save_file| save_file.puts(yaml)}
     puts("Game saved.")
-    save_file.close
+  end
+
+  def save_file_count
+    Dir.glob("#{SAVE_FILE_DIR}/save*.yaml").length
   end
 
   def reveal_letters(guess)
@@ -86,6 +91,17 @@ class Game
 
 end
 
+def get_save_files
+  Dir.glob("saves/save*.yaml")
+end
+
+def show_save_files
+  save_files = get_save_files
+  save_files.each_with_index do |file, i|
+    puts("[#{i}] - #{file}")
+  end
+end
+
 def show_menu
   puts("[1] START GAME")
   puts("[2] LOAD GAME")
@@ -96,6 +112,15 @@ def get_menu_input
     print(">> ")
     input = gets.chomp
     return input if input == "1" || input == "2"
+  end
+end
+
+def get_load_save_input
+  save_files = get_save_files.length
+  loop do
+    print(">> ")
+    input = get.chomp
+    return input if input.to_i <=save_files && input >= 1
   end
 end
 
@@ -112,7 +137,9 @@ def setup
     secret_word = get_secret_word(dictionary, 1, 12)
     Game.new({secret_word: secret_word})
   when "2"
-    Game.load_game
+    show_save_files
+
+    # Game.load_game
   end
 end
 
